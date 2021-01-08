94c713c1f7c2ef4d9e8fb9e17ae82e3b72796c2d
jiangxin <jiangxin792@sina.com>
2021-01-08 09:17:17 +0800

    dd

---
+++ project/arithmetic-exercise/src/main/java/com/chiangshin/arithmetic/exercise/MissingNumber.java
@@ -0,0 +1,7 @@
+package com.chiangshin.arithmetic.exercise;
+
+public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        return 0;
+    }
+}
---
+++ project/arithmetic-exercise/src/main/java/com/chiangshin/arithmetic/exercise/PrintNumbers.java
@@ -0,0 +1,13 @@
+package com.chiangshin.arithmetic.exercise;
+
+public class PrintNumbers {
+    public int[] printNumbers(int n) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int n = 8;
+        double nums = Math.pow(3.0,3.0);
+        System.out.println(nums);
+    }
+}
